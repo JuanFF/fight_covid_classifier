9b4049c9f74c1e56dce803e30a2ac60b878420be
Juan Fernández <jnferfer1@gmail.com>
2020-05-29 00:57:57 +0200

    remove tweepy

--- Tweepy/post.py
+++
@@ -1,8 +0,0 @@
-import tweepy, twitter_keys
-
-auth = tweepy.OAuthHandler(twitter_keys.consumer_key, twitter_keys.consumer_secret) 
-auth.set_access_token(twitter_keys.access_token, twitter_keys.access_token_secret)
-api = tweepy.API(auth)
-
-
-api.update_status('¡Hola! Hoy es domingo 10 de mayo, del 2020')
