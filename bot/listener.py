126371befe56d431b52e2fe8f05910d8b387d40e
Juan Fernández <jnferfer1@gmail.com>
2020-06-05 18:41:56 +0200

    Add new new module 'bot' that for streaming and posting on Twitter

---
+++ bot/listener.py
@@ -0,0 +1,155 @@
+import sys
+sys.path.append('../')
+import tweepy, twitter_keys
+from annotator_pkg import annotator
+from classifier_pkg import classifier
+import time
+import re
+
+
+queries = ['covid', 'covid-19', 'coronavirus', 'sars-cov-2']
+queueFilePath = 'queue.txt'
+
+
+def writeLogFile (label, status):
+	
+	if status.user.verified:
+		twitterVerified = 'twitter_verified'
+	else:
+		twitterVerified = 'not_twitter_verified'
+
+	if status.user.description:
+		description = status.user.description
+		description = description.replace('\n', ' ')
+	else:
+		description = 'no_description'
+
+	with open('log.tsv', 'a', encoding = 'utf-8') as logFile:
+		logFile.write(label + '\t' + status.text + '\t' + twitterVerified + '\t' + description + '\n')
+
+		
+def checkTooSimilar (inputString, queueFilePath):
+
+	def cleanseString (string):
+		string = re.sub('\s*http.+', '', string)
+		string = re.sub('\s+vía http.+', '', string)
+		string = re.sub('^RT @\S+\: ', '', string)
+		string = string.lower()
+		return string
+
+	tooSimilar = False
+
+	inputString = cleanseString(inputString)
+
+	with open(queueFilePath, 'r', encoding = 'utf-8') as queueFile:
+		queue = [line.split('\t', 1)[1].rstrip().lower() for line in queueFile.readlines()]
+
+	from difflib import SequenceMatcher
+	for message in queue:
+		message = cleanseString(message)
+		if SequenceMatcher(None, inputString, message).ratio() > 0.75:
+			tooSimilar = True
+			break
+	
+	return tooSimilar
+
+
+def analyzeUser (status):
+
+	# user is valid if account is 'Twitter verified'
+	if status.user.verified == True:
+		return True
+
+	else:
+		# must have 'description'
+		description = status.user.description
+		if description:
+
+			# classifier doesn't work with '\n'
+			if '\n' in description:
+				return False
+			
+			# string must be long enough to be annotated
+			descriptionAnnResult = annotator.annotator(description, '../annotator_pkg/lang_data/isSpecialist_nodes.json')
+			if descriptionAnnResult:
+
+				# classifier's label must be 'isSpecialist'
+				label = classifier.classify(description_model, descriptionAnnResult[0])
+				if label == 'isSpecialist':
+					return True
+
+		return False
+
+
+def findQueries (queries, string):
+
+	for query in queries:
+		if query in string.lower():
+			return True
+
+	return False
+
+
+def analyzeTweet (status, queries, queueFilePath):
+
+	message = status.text
+
+	# classifier doesn't work with '\n'
+	if '\n' in message:
+		return False
+
+	# message must have queries
+	if findQueries(queries, message) is False:
+		return False
+	
+	# user must be Twitter verified or categorized as 'isSpecialist' (=True)
+	userAnalysis = analyzeUser(status)
+	if userAnalysis is True:
+
+		# message must be less than 0.75 similar to queue's messages
+		if checkTooSimilar(message, queueFilePath):
+			return False
+
+		# string must be long enough to be annotated
+		messageAnnResult = annotator.annotator(message, '../annotator_pkg/lang_data/defeatVirus_nodes.json')
+		if messageAnnResult:
+			
+			# write analysis to log file
+			label = classifier.classify(message_model, messageAnnResult[0])
+			# writeLogFile(label, status)
+
+			# classifier's label must be 'defeat'
+			if label == 'defeat':
+				return True
+
+	return False
+
+
+class CustomStreamListener(tweepy.StreamListener):
+
+	def on_status(self, status):
+		if status.lang == 'es':
+			with open('queue.txt', 'a', encoding = 'utf-8') as queueFile:
+				analysis = analyzeTweet(status, queries, queueFilePath)
+				if analysis is True:
+					queueFile.write(status.id_str + '\t' + status.user.screen_name + '\t' + status.text + '\n')
+
+	
+auth = tweepy.OAuthHandler(twitter_keys.consumer_key, twitter_keys.consumer_secret) 
+auth.set_access_token(twitter_keys.access_token, twitter_keys.access_token_secret)
+api = tweepy.API(auth) 
+
+
+description_model = classifier.loadModel('../classifier_pkg/isSpecialist.train.txt')
+message_model = classifier.loadModel('../classifier_pkg/defeatVirus.train.txt')
+
+
+while True:
+	try:
+		print('\n' + 'Bot listening...')
+		sapi = tweepy.streaming.Stream(auth, CustomStreamListener())
+		sapi.filter(track = queries)
+	except:
+		print('\n' + 'Pause in error...')
+		time.sleep(25)
+		continue
---
+++ bot/post.py
@@ -0,0 +1,35 @@
+import tweepy, twitter_keys
+import time
+
+
+auth = tweepy.OAuthHandler(twitter_keys.consumer_key, twitter_keys.consumer_secret) 
+auth.set_access_token(twitter_keys.access_token, twitter_keys.access_token_secret)
+api = tweepy.API(auth) 
+
+
+print('Bot publishing...')
+
+
+while True:
+
+	with open('posted_ids.txt', 'r', encoding = 'utf-8') as postedIdsFile:
+		postedIds = [line.rstrip() for line in postedIdsFile]
+
+	with open('queue.txt', 'r', encoding = 'utf-8') as queueFile:
+		queue = [line.rstrip() for line in queueFile.readlines()]
+
+	count = 0
+	for tweet in queue:
+		count += 1
+		tweetId = tweet.split('\t', 2)[0]
+		if tweetId not in postedIds:
+			message = tweet.split('\t', 2)[2]
+			screenName = tweet.split('\t', 2)[1]
+			newStatus = 'https://twitter.com/' + screenName + '/status/' + tweetId
+			api.update_status(newStatus)
+			with open('posted_ids.txt', 'a', encoding = 'utf-8') as postedIdsFile:
+				postedIdsFile.write(tweetId + '\n')
+			print('\n[' + str(len(queue) - count) + ' left] ' + message)
+			break
+	
+	time.sleep(900)
